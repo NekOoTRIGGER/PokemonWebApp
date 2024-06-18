2367c1d8ec2351d30eea673eb3f8dc32fd4a0255
Stephane Harquel <harquel.stephane@hotmail.fr>
2024-06-18 23:45:06 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon/pokemon';
+import { POKEMONS } from './pokemon/mock-pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  pokemonList: Pokemon[] = POKEMONS;
+  ngOnInit(): void {
+    console.log(this.pokemonList[0]);
+  }
   title = 'pokemon-app';
 }
